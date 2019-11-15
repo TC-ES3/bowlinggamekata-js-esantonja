35fe855e71b0b22644c7031bf266ee20dd9cdde1
Kike <enrique.santonja@travelport.com>
2019-11-15 16:15:35 +0100

    feat: implement strike implementation

--- bowling.js
+++ bowling.js
@@ -10,13 +10,17 @@ export default class Game {
         let score = 0;
         let frameIndex = 0;
         for(let frame = 0; frame < 10; frame+=1) {
-            if(isSpare(rolls, frameIndex))
+            if(rolls[frameIndex] === 10) {
+                // strike
+                score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                frameIndex += 1;
+            } else if(isSpare(rolls, frameIndex))
             {
-                score += rolls[frameIndex] + rolls[frameIndex + 1] + rolls[frameIndex + 2];
-                frameIndex += 2
+                score += 10 + rolls[frameIndex + 2];
+                frameIndex += 2;
             } else {
                 score += rolls[frameIndex] + rolls[frameIndex + 1];
-                frameIndex += 2
+                frameIndex += 2;
             }
         }
 
