a22759c9e32e9d10a830ef71267f0b9171612b93
Kike <enrique.santonja@travelport.com>
2019-11-15 15:58:11 +0100

    test: fix one spare

--- bowling.js
+++ bowling.js
@@ -9,9 +9,16 @@ export default class Game {
         const rolls = this._rolls;
         let score = 0;
         let i = 0;
-        for(let frame = 0; frame < 10; frame++) {
-            score += rolls[i] + rolls[i + 1];
-            i += 2
+        for(let frame = 0; frame < 10; frame+=1) {
+            if(rolls[i] + rolls[i + 1] === 10)
+            {
+                //spare
+                score += rolls[i] + rolls[i + 1] + rolls[i + 2];
+                i += 2
+            } else {
+                score += rolls[i] + rolls[i + 1];
+                i += 2
+            }
         }
 
         return score;
--- bowling.spec.js
+++ bowling.spec.js
@@ -21,9 +21,10 @@ test("all ones", () => {
   expect(game.score()).toBe(20);
 });
 
-// test("one spare", () => {
-//   game.roll(5);
-//   game.roll(5); // spare
-//   game.roll(3);
-//   expect(game.score()).toBe(16);
-// });
+test("one spare", () => {
+  game.roll(5);
+  game.roll(5); // spare
+  game.roll(3);
+  rollMany(17, 0);
+  expect(game.score()).toBe(16);
+});
